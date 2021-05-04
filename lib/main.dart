ce81bcc8bb987366562e8c01acb715ed32c92e61
sooraj <sbkaiprath99@gmail.com>
2021-05-05 01:42:09 +0530

    login page complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import './screens/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,156 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-     
-  
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- 
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-        Container(
-          child: Stack(children: [
-            Container(
-              child: Text("Welcome Back",style: TextStyle(fontSize: 28,fontWeight:FontWeight.w600,
-              color:Color.fromRGBO(96, 56, 32, 1),)
-              ),
-              padding:EdgeInsets.fromLTRB(25.01, 112.61,24.99, 0),
-        
-              ),
-             Container(
-              child: Text("signup or login to see\nour top pics for you",style: TextStyle(fontSize: 17,fontWeight:FontWeight.w300,
-              color:Color.fromRGBO(96, 56, 32, 1),)
-              ),
-              padding:EdgeInsets.fromLTRB(25.01, 161,24.99, 0),
-        
-              ),
-          ],
-          ),
-        ),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-         
-       children:[
-          Container(
-              child: Text("Email Address",style: TextStyle(fontSize: 15,fontWeight:FontWeight.w600,
-              color:Color.fromRGBO(96, 56, 32, 1),)
-              ),
-     
-                  padding:EdgeInsets.fromLTRB(25.01,44,24.99, 0)
-              ),
-Container(
- 
-    
-      padding: EdgeInsets.only(left:25,top: 15,right: 25,bottom: 20),
-      child:  TextField(
-        textAlign: TextAlign.center,
-        decoration: InputDecoration(
-  hintText: "Hint text sample",
-  border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(5.0),
-    borderSide: BorderSide(
-      color: Colors.amber,
-      style: BorderStyle.solid,
-    ),
-  ),
-),
-
-      ),
-    ),
-    Container(
-              child: Text("Password",style: TextStyle(fontSize: 15,fontWeight:FontWeight.w600,
-              color:Color.fromRGBO(96, 56, 32, 1),)
-              ),
-     
-                  padding:EdgeInsets.fromLTRB(25.01,20,24.99, 0)
-              ),
-Container(
- 
-    
-      padding: EdgeInsets.only(left:25,top: 15,right: 25,bottom: 20),
-      child:  TextField(
-        obscureText: true,
-        textAlign: TextAlign.center,
-        decoration: InputDecoration(
-  hintText: "Hint text sample",
-  border: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(5.0),
-    borderSide: BorderSide(
-      color: Colors.amber,
-      style: BorderStyle.solid,
-    ),
-  ),
-),
-
-      ),
-    ),
-   SizedBox(
-     width: 400,
-     height: 90,
-     child: Container(
-      
-        padding:EdgeInsets.fromLTRB(25, 20, 25, 20),
-        child: RaisedButton(
-                    child: Text('Login',style: TextStyle(color:Colors.white70),),
-                    color:Color.fromRGBO(106, 147, 71, 1),
-                    onPressed: () {},
-                  ),
-     ),
-   ),
-   Center(
-     child: Text("Forgot password ?",style: TextStyle(color: Colors.blue),),
-   ),
-      Padding(
-        
-        padding: const EdgeInsets.only(top: 40,bottom: 30),
-        child: Center(
-     child: Text("Or login with",style: TextStyle(color: Color.fromRGBO(96, 56, 32, 1),),),
-   ),
-      ),
-      SizedBox(
-     width: 400,
-     height: 90,
-     child: Container(
-      
-        padding:EdgeInsets.fromLTRB(25, 20, 25, 20),
-        child: RaisedButton.icon(
-                    icon: Icon(MyFlutterApp.google),
-                    label: Text('Continue with google',style: TextStyle(color:Colors.white70),),
-                    color:Color.fromRGBO(225, 160, 103, 1),
-                    onPressed: () {},
-                    
-                  ),
-     ),
-   ),
-   
-],
-
-          ),
-        )
-      ],)
-   
+      theme: ThemeData(fontFamily: 'Lato'),
+      debugShowCheckedModeBanner: false,
+      title: 'Elegant Furnitures',
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/screens/login.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  var _passwordFocus = FocusNode();
+  var _loginFocus = FocusNode();
+  var _obscureText = true;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _passwordFocus.dispose();
+    _loginFocus.dispose();
+  }
+
+  void _toggle() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  void _next() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    //_formKey.currentState.save();
+    setState(() {
+      _obscureText = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+          height: MediaQuery.of(context).size.height * 1,
+          width: double.infinity,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.13),
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                          color: Colors.brown,
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                    Text('Signing up or \nlogin to see our top picks for you.',
+                        style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300)),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.only(right: 30),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Email Address',
+                                      style: TextStyle(
+                                          color: Colors.brown,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.015),
+                                  TextFormField(
+                                    textInputAction: TextInputAction.next,
+                                    decoration: InputDecoration(
+                                        hintText: 'Email Address',
+                                        border: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.brown[300]),
+                                            borderRadius:
+                                                BorderRadius.circular(7))),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "Email Id not entered";
+                                      }
+                                      return null;
+                                    },
+                                    onFieldSubmitted: (value) {
+                                      FocusScope.of(context)
+                                          .requestFocus(_passwordFocus);
+                                    },
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.015),
+                                  Text('Password',
+                                      style: TextStyle(
+                                          color: Colors.brown,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold)),
+                                  Column(
+                                    children: [
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.015),
+                                      TextFormField(
+                                          textInputAction: TextInputAction.done,
+                                          focusNode: _passwordFocus,
+                                          obscureText: _obscureText,
+                                          decoration: InputDecoration(
+                                            hintText: 'Password',
+                                            border: OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.brown[300]),
+                                                borderRadius:
+                                                    BorderRadius.circular(7)),
+                                            suffixIcon: IconButton(
+                                              icon: Icon(_obscureText
+                                                  ? Icons.lock
+                                                  : Icons.lock_open),
+                                              onPressed: _toggle,
+                                            ),
+                                          ),
+                                          validator: (value) {
+                                            if (value.isEmpty) {
+                                              return "Password not entered";
+                                            }
+                                            return null;
+                                          },
+                                          onFieldSubmitted: (value) {
+                                            FocusScope.of(context)
+                                                .requestFocus(_loginFocus);
+                                            _next();
+                                          }),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.03),
+                                      Container(
+                                        height: 50,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                backgroundColor:
+                                                    MaterialStateProperty.all<
+                                                        Color>(Colors.green),
+                                                elevation: MaterialStateProperty
+                                                    .all<double>(5)),
+                                            onPressed: _next,
+                                            child: Container(
+                                              width: double.infinity,
+                                              child: Text(
+                                                'Login',
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    color: Colors.white),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                            )),
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.03),
+                                      Text(
+                                        'Forgot Password?',
+                                        style: TextStyle(
+                                            fontSize: 17,
+                                            color: Colors.blueGrey),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.07),
+                                      Text(
+                                        'Or Login with',
+                                        style: TextStyle(
+                                            fontSize: 17, color: Colors.brown),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.02),
+                                      Container(
+                                        height: 50,
+                                        child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                backgroundColor:
+                                                    MaterialStateProperty.all<
+                                                            Color>(
+                                                        Colors.deepOrange),
+                                                elevation: MaterialStateProperty
+                                                    .all<double>(5)),
+                                            onPressed: () => {},
+                                            child: Container(
+                                              width: double.infinity,
+                                              child: Row(
+                                                children: [
+                                                  Icon(
+                                                    MdiIcons.google,
+                                                    size: 30,
+                                                  ),
+                                                  SizedBox(
+                                                      width:
+                                                          MediaQuery.of(context)
+                                                                  .size
+                                                                  .width *
+                                                              0.1),
+                                                  Text(
+                                                    'Continue with Google',
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        color: Colors.white),
+                                                    textAlign: TextAlign.center,
+                                                  ),
+                                                ],
+                                              ),
+                                            )),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ))
+                        ],
+                      ),
+                    ),
+                  ]))),
+    ));
+  }
+}
