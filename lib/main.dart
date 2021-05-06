5ed245af6791fb348f222b6535c3edd097810681
sooraj <sbkaiprath99@gmail.com>
2021-05-07 00:47:25 +0530

    connected firebase & home page modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'screens/login.dart';
 import './screens/create_account_screen.dart';
 import './screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
